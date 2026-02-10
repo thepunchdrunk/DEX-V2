4475264274c35c873bb3f3d38f2a93951d0441d7
rvm_danfoss <rvm@danfoss.com>
2026-02-10 13:12:06 +0530

    Configure base path for DEX-V2 deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
   return {
-    base: mode === 'production' ? '/DEX-V1/' : '/',
+    base: mode === 'production' ? '/DEX-V2/' : '/',
     server: {
       host: '0.0.0.0',
     },
